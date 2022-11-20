f7b926a4ba82d20dd7df587fe2f01b120e2cdf1a
TatsuSolo <1730150@iub.edu.bd>
2022-11-20 18:55:41 +0600

    office - 11-20-22

--- app/src/main/java/com/foysal/practice/databasepractice/ViewModel/NoteViewModel.kt
+++ app/src/main/java/com/foysal/practice/databasepractice/ViewModel/NoteViewModel.kt
@@ -1,4 +1,22 @@
 package com.foysal.practice.databasepractice.ViewModel
 
-class NoteViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.foysal.practice.databasepractice.Model.Note
+import com.foysal.practice.databasepractice.Repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(private val repository: NoteRepository) : ViewModel(){
+
+    val myAllNotes : LiveData<List<Note>> = repository.myAllNote.asLiveData()
+
+    fun insert(note: Note) = viewModelScope.launch(Dispatchers.IO){
+
+
+
+    }
+
 }
